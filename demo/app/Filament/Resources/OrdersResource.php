2787a34adcdb2c02248d7effa5d707e59d418b03
Bansal Patel <htcexplorer2412@gmail.com>
2024-11-29 17:13:12 +1100

    daily commit

--- demo/app/Filament/Resources/OrdersResource.php
+++ demo/app/Filament/Resources/OrdersResource.php
@@ -4,7 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\OrdersResource\Pages;
 use App\Filament\Resources\OrdersResource\RelationManagers;
-use App\Models\Orders;
+use App\Models\orders;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
@@ -13,18 +13,56 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 
+
+use Filament\Forms\Components\Section;
+use Filament\Forms\Component\PlaceHolder;
+
+
 class OrdersResource extends Resource
 {
-    protected static ?string $model = Orders::class;
+    protected static ?string $model = orders::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
+    protected static ?int $navigationSort = 4;
+
+    protected static ?string $navigationGroup = 'Shop';
 
     public static function form(Form $form): Form
     {
         return $form
-            ->schema([
-                //
-            ]);
+        ->schema([
+                    
+
+                    Section::make('Order items')
+                        ->headerActions([
+                            Action::make('reset')
+                                ->modalHeading('Are you sure?')
+                                ->modalDescription('All existing items will be removed from the order.')
+                                ->requiresConfirmation()
+                                ->color('danger')
+                                ->action(fn (Forms\Set $set) => $set('items', [])),
+                        ])
+                        ->schema([
+                            static::getItemsRepeater(),
+                        ])
+                
+                ->columnSpan(['lg' => fn (?Order $record) => $record === null ? 3 : 2]),
+
+            Section::make()
+                ->schema([
+                    Placeholder::make('created_at')
+                        ->label('Created at')
+                        ->content(fn (Order $record): ?string => $record->created_at?->diffForHumans()),
+
+                    Placeholder::make('updated_at')
+                        ->label('Last modified at')
+                        ->content(fn (Order $record): ?string => $record->updated_at?->diffForHumans()),
+                ])
+                ->columnSpan(['lg' => 1])
+                ->hidden(fn (?Order $record) => $record === null),
+        ])
+        ->columns(3);
     }
 
     public static function table(Table $table): Table
